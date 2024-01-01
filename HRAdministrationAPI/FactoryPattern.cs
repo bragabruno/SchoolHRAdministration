1d890d7f4238a86fdce9808ca238dc387fa04773
Bruno Braga <bragabruno@users.noreply.github.com>
2023-12-31 22:05:17 -0300

    Factory Design Pattern

---
+++ HRAdministrationAPI/FactoryPattern.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace HRAdministrationAPI
+{
+	public class FactoryPattern<K, T> where T:class,K,new()
+	{
+		public static K GetInstance()
+		{
+			K objK;
+			objK = new T();
+			return objK;
+		}
+	}
+}
+
